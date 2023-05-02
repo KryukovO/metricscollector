8013e10649d085d520b05ed03d8ab332a540124c
Oleg Kryukov <ol_kryukov97@mail.ru>
2023-05-02 22:24:04 +0300

    add flag -a to server app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 
-	"github.com/KryukovO/metricscollector/internal/server"
-	"github.com/KryukovO/metricscollector/internal/storage/memstorage"
+	"github.com/KryukovO/metricscollector/internal/server/config"
+	"github.com/KryukovO/metricscollector/internal/server/server"
 )
 
 func main() {
-	if err := server.Run(memstorage.New()); err != nil {
+	var c config.Config
+	flag.StringVar(&c.HTTPAddress, "a", "127.0.0.1:8080", "Server endpoint address (host:port)")
+	flag.Parse()
+
+	if err := server.Run(&c); err != nil {
 		log.Fatalf("server error: %s. Exit(1)\n", err.Error())
 	}
 }
---
+++ internal/server/config/config.go
@@ -0,0 +1,5 @@
+package config
+
+type Config struct {
+	HTTPAddress string
+}
