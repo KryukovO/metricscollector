56765132a936af0523b3677f99c0e846e4054a3b
Oleg Kryukov <ol_kryukov97@mail.ru>
2023-05-18 22:30:31 +0300

    add extended log to logging middleware

--- internal/server/middleware/middleware.go
+++ internal/server/middleware/middleware.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"time"
+
 	"github.com/labstack/echo"
 	log "github.com/sirupsen/logrus"
 )
@@ -8,6 +10,16 @@ import (
 func LoggingMiddlewarefunc(next echo.HandlerFunc) echo.HandlerFunc {
 	return echo.HandlerFunc(func(e echo.Context) (err error) {
 		log.Infof("recieved query with method %s: %s", e.Request().Method, e.Request().RequestURI)
-		return next(e)
+
+		ts := time.Now()
+		res := next(e)
+
+		if res != nil {
+			log.Infof("query response status: %d; size: %d; duration: %s", res.(*echo.HTTPError).Code, e.Response().Size, time.Since(ts))
+		} else {
+			log.Infof("query response status: %d; size: %d; duration: %s", e.Response().Status, e.Response().Size, time.Since(ts))
+		}
+
+		return res
 	})
 }
